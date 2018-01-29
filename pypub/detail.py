4f10a2b5e5c0dd69d8bfff123d78b4344e221eb9
zengkv <740832946@qq.com>
2018-01-29 17:24:47 +0800

    实现版本详情页

--- pypub/detail.py
+++ pypub/detail.py
@@ -9,29 +9,67 @@ class Detail(object):
         pass
 
     def render(self, infos, errmsg):
-    	render = WEB_T.get_render()
-    	session = WEB_T.get_session()
-    	return render.detail(session.username, infos, errmsg)
+        render = WEB_T.get_render()
+        session = WEB_T.get_session()
+        return render.detail(session.username, infos, errmsg)
 
     def get_infos(self):
-    	i = web.input()
-    	appid = i.get('appid')
-    	db = COMMON.get_db()
-    	appver = COMMON.dbget("cur-%s" % appid, None, "json")
-    	if not appver:
-    		return [], "appid错误"
-    	page = i.get("page")
-    	if not page:
-    		page = 1
-    	appver.setdefault("history", [])
-    	infos = {"vers": {}, "history": appver["history"]}
-    	for ver in appver["history"]:
-    		key = "ver-%s-%s" % (appid, ver)
-    		infos["vers"][ver] = COMMON.dbget(key, None, "json")
-    	return infos, ""
+        i = web.input()
+        appid = i.get('appid')
+        db = COMMON.get_db()
+        appver = COMMON.dbget("cur-%s" % appid, None, "json")
+        if not appver:
+            return {"vers": {}, "history": []}, "appid错误"
+        page = i.get("page")
+        try:
+            page = int(page)
+        except:
+            page = 1
+        appver.setdefault("history", [])
+        infos = {"vers": {}, "history": appver["history"]}
+        for ver in appver["history"]:
+            key = "ver-%s-%s" % (appid, ver)
+            infos["vers"][ver] = COMMON.dbget(key, None, "json")
+        infos["history"].reverse()
+        start = (page-1)*20
+        end = page*20
+        count = len(infos["history"])
+
+        for i in range(start, end):
+            if i >= len(infos["history"]):
+                break
+            ver = infos["history"][i]
+            verinfo = infos["vers"][ver]
+            infos["vers"][ver]["newhtm"] = ""
+            for file in verinfo["update"]["new"]:
+                infos["vers"][ver]["newhtm"] += "<a href='codediff?appid=%s&src=%s&v1=%s&v2=now'>%s</a><br>" % (appid, file, ver, file)
+
+            infos["vers"][ver]["modifyhtm"] = ""
+            for file in verinfo["update"]["modify"]:
+                v2 = 'now'
+                for j in range(i+1, count):
+                    v = infos["history"][j]
+                    vinfo = infos["vers"][v]
+                    if file in vinfo["update"]["new"] or file in vinfo["update"]["modify"]:
+                        v2 = v
+                        break
+                infos["vers"][ver]["modifyhtm"] += "<a href='codediff?appid=%s&src=%s&v1=%s&v2=%s'>%s</a><br>" % (appid, file, ver, v2, file)
+            
+            infos["vers"][ver]["delhtm"] = ""
+            for file in verinfo["update"]["del"]:
+                v2 = "now"
+                for j in range(i+1, count):
+                    v = infos["history"][j]
+                    vinfo = infos["vers"][v]
+                    if file in vinfo["update"]["new"] or file in vinfo["update"]["modify"]:
+                        v2 = v
+                        break
+                infos["vers"][ver]["delhtm"] += "<a href='codediff?appid=%s&src=%s&v1=%s&v2=%s'>%s</a><br>" % (appid, file, v2, 'now', file)
+
+        infos["history"] = infos["history"][start:end]
+        return infos, ""
 
     def GET(self):
         WEB_T.check_login()
-    	infos, errmsg = self.get_infos();
-    	print infos
-    	return self.render(infos, errmsg)
+        infos, errmsg = self.get_infos();
+        return self.render(infos, errmsg)
