dc2a5d9e0b8e1d87c566f55799748e4f89b302e3
zengkv <740832946@qq.com>
2018-01-31 16:14:51 +0800

    daemon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #coding=utf-8
 session = None
 import web
+import sys
+import os
 from pypub.tools import CONFIG_T, WEB_T
 from pypub.index import Index
 from pypub.login import Login
@@ -10,25 +12,30 @@ from pypub.downfile import Downfile
 from pypub.syncdetail import Syncdetail
 
 def main():
-	global session
-	config = CONFIG_T.load_config()
-	if not config:
-		print("配置文件加载失败！")
-		return
-	CONFIG_T.init_logger()
-	CONFIG_T.load_ssl()
-	urls = (
-		"/", "Index",
-		"/detail", "Detail",
-		"/login", "Login",
-		"/codediff", "Codediff",
-		"/downfile", "Downfile",
-		"/syncdetail", "Syncdetail"
-		)
-	web.config.debug = False
-	app = web.application(urls, globals())
-	session = WEB_T.get_session(app)
-	app.run()
+    global session
+    config = CONFIG_T.load_config()
+    if not config:
+        print("配置文件加载失败！")
+        return
+    CONFIG_T.init_logger()
+    CONFIG_T.load_ssl()
+    urls = (
+        "/", "Index",
+        "/detail", "Detail",
+        "/login", "Login",
+        "/codediff", "Codediff",
+        "/downfile", "Downfile",
+        "/syncdetail", "Syncdetail"
+        )
+    web.config.debug = False
+    app = web.application(urls, globals())
+    session = WEB_T.get_session(app)
+    app.run()
 
 if __name__ == '__main__':
-	main()
+
+    if 'daemon' in sys.argv[1:]:
+        pid = os.fork()
+        if pid != 0 : quit()
+
+    main()
